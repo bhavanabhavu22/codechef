cd341902c2307d228ca8a1d94fd7d2f26fafeff1
bhavanabhavu22 <bhavanakallibandla@gmail.com>
2024-12-10 06:33:12 +0530

    Add files via upload

---
+++ NUGGET.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class NUGGET
+{
+	public static void main (String[] args) 
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int s=sc.nextInt();
+		int a=(n*5000 + s*9800);
+		System.out.println(a);
+
+	}
+}
+
