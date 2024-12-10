51142a38282f52fe7c1816d05fec0c3efd51b6a4
bhavanabhavu22 <bhavanakallibandla@gmail.com>
2024-12-10 06:43:17 +0530

    Add files via upload

---
+++ chefANDparloe.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class chefANDparloe
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		if(a>=7)
+		System.out.println("yes");
+		else
+		System.out.println("no");
+
+	}
+}
+
