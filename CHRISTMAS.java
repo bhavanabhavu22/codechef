a0a997d02dc3d3af60394ba0b2ed8471841459a8
bhavanabhavu22 <bhavanakallibandla@gmail.com>
2024-12-10 06:32:53 +0530

    Add files via upload

---
+++ CHRISTMAS.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class CHRISTMAS
+{
+	public static void main (String[] args) 
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		if((a%2==1))
+		{
+		    System.out.println("CHRISTMAS");
+		}
+		    else
+		    {
+		        System.out.println("ORDINARY");
+
+		    }"
+		}
+	}	
