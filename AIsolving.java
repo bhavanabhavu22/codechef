0b28cfa369cbe3509468bcc2e3d9238af06b7812
bhavanabhavu22 <bhavanakallibandla@gmail.com>
2024-12-10 06:32:12 +0530

    Add files via upload

---
+++ AIsolving.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class AIsolving
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+	    if(n<=60)
+	    {
+	        System.out.println("YES");
+	    }
+	    else
+	    {
+	        System.out.println("NO");
+	    }
+	}
+}
+
