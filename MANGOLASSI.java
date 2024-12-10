0cfc14a06fe68963d0ec11d6f09f0a1d12e2c716
bhavanabhavu22 <bhavanakallibandla@gmail.com>
2024-12-10 06:43:43 +0530

    Add files via upload

---
+++ MANGOLASSI.java
@@ -0,0 +1,15 @@
+import java.util.*;
+ public class MANGOLASSI
+{
+	public static void main(String[] args) 
+	{
+		// your code goes hereyes")
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n>35)
+		System.out.println("yes");
+		else
+		System.out.println("no");
+
+	}
+}
