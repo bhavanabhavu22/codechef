bc8c15ea5b4fa19b0a20bd74b4b845be34c22764
bhavanabhavu22 <bhavanakallibandla@gmail.com>
2024-12-12 21:37:00 +0530

    Add files via upload

---
+++ MIRROR.java
@@ -0,0 +1,15 @@
+import java.util.Scanner; 
+class MIRROR
+{
+	public static void main (String[] args)
+	{
+	    // Do not print anything here before taking input
+	    Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println(n);
+		
+		// Print the value of n
+		
+	}
+}
+
