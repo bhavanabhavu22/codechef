b8d99ab7f27b31e85174f83770b61e04b17cf732
bhavanabhavu22 <bhavanakallibandla@gmail.com>
2024-12-12 21:26:10 +0530

    Add files via upload

---
+++ ELECTIONHOPES.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class ELECTIONHOPES
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		int y=sc.nextInt();
+		if (x>=2*y)
+		{
+		    System.out.println("yes");
+		   } else{
+		        System.out.println("no");
+		   }
+	}
+}
