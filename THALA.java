050625da5c477551851727c7e36a5d0fe0f79e83
bhavanabhavu22 <bhavanakallibandla@gmail.com>
2024-12-12 21:29:20 +0530

    Add files via upload

---
+++ THALA.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class THALA
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+         Scanner sc=new Scanner(System.in);
+         int n=sc.nextInt();
+         if(n==7)
+         System.out.println("THALA");
+         else
+         System.out.println("sadge");
+	}
+}
+
