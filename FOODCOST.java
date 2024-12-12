3804b06ee7022ab98e68088b38f388b3c20753cf
bhavanabhavu22 <bhavanakallibandla@gmail.com>
2024-12-12 21:38:46 +0530

    Add files via upload

---
+++ FOODCOST.java
@@ -0,0 +1,14 @@
+import java.util.*;
+ class FOODCOST
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int z=6*x+y;
+         System.out.println(z);
+         
+    }
+}
+
