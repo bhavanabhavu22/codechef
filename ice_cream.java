6b1a2dd874b1d9032db0db879f7ae227288f795a
bhavanabhavu22 <bhavanakallibandla@gmail.com>
2024-12-14 21:02:45 +0530

    Add files via upload

---
+++ ice_cream.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class ice_cream
+{
+	public static void main (String[] args) 
+	{
+	    Scanner sc=new Scanner(System.in);
+        int X=sc.nextInt(); 
+        int Y=sc.nextInt(); 
+        int max=Math.min(X, Y);
+        System.out.println(max);
+	}
+}
