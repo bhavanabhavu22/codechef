af24dabca4223c4a8a7cdb29a473a02ff5ec1494
bhavanabhavu22 <bhavanakallibandla@gmail.com>
2024-12-14 21:27:21 +0530

    Add files via upload

---
+++ codeandsocks.java
@@ -0,0 +1,24 @@
+
+   
+    import java.util.*;
+import java.lang.*;
+import java.io.*;
+class chefandsocks
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 Scanner sc=new Scanner(System.in);
+        int X=sc.nextInt();
+        // Check if the points are greater than or equal to 12
+        if (X >= 12) 
+        {
+            System.out.println("Yes");
+        }
+        else 
+        {
+            System.out.println("No");
+        }
+        sc.close();
+	}
+}
+}
