9b774ec7de1c0379c226a5ed7175bc812f657814
bhavanabhavu22 <bhavanakallibandla@gmail.com>
2024-12-14 20:52:00 +0530

    Add files via upload

---
+++ moneymaking.java
@@ -0,0 +1,14 @@
+
+
+import java.util.*;
+class MoneyMaking
+{
+	public static void main (String[] args) 
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int X=sc.nextInt();
+	    int Y=sc.nextInt();
+		int Money=(X*5000)+(Y*9800);
+		System.out.println(Money);
+	}
+}
